93594c4695a991067dfee5be382395e2efdd0db8
Turtleman <seungkyu0215@gmail.com>
2024-06-01 09:46:12 +0900

    5일차

---
+++ 수학/1373_진수.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const input = fs.readFileSync('../example.txt').toString().trim().split('\n')
+
+const num = input[0]
+let result = ''
+const getNum = (idx) => num[idx] ?? ''
+for(let i = num.length-1; i > -1; i-=3) {
+    const octalNum = parseInt(`${getNum(i-2)}${getNum(i-1)}${getNum(i)}`,2).toString(8)
+    result = octalNum + result
+}
+console.log(result)
+
