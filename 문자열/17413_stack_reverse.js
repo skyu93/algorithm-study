7bef47ec6604a3e203f170468239f0c5e5ca76b0
Turtleman <seungkyu0215@gmail.com>
2024-06-04 06:45:49 +0900

    정리 수정

--- 문자열/17413_stack_reverse.js
+++ 문자열/17413_stack_reverse.js
@@ -1,54 +1,19 @@
 const fs = require('fs');
 const input = fs.readFileSync('../example.txt').toString().split('\n');
-const answer = (str) => {
-    let result = ''
-    let word = []
-    let inTag = false
-
-    const addReversedWordToResult = () => {
-        if (word.length > 0) {
-            result += word.reverse().join('');
-            word = [];
-        }
-    };
-
-    for (let i = 0; i < str.length; i++) {
-       const s = str[i]
-        if(s === '<') {
-            inTag = true
-            addReversedWordToResult()
-            result += s
-        } else if(s === '>') {
-            inTag = false
-            result += s
-        } else if(s === ' '){
-            addReversedWordToResult()
-            result += s
-        } else {
-            if(inTag) {
-                result += s
-            }else {
-                word.push(s)
-            }
-        }
-    }
-    addReversedWordToResult()
-    return result
-};
-
 // const answer = (str) => {
 //     let result = ''
-//     let stack = []
+//     let word = []
 //     let inTag = false
 //
 //     const addReversedWordToResult = () => {
-//         while (stack.length > 0) {
-//             result += stack.pop()
+//         if (word.length > 0) {
+//             result += word.reverse().join('');
+//             word = [];
 //         }
 //     };
 //
 //     for (let i = 0; i < str.length; i++) {
-//         const s = str[i]
+//        const s = str[i]
 //         if(s === '<') {
 //             inTag = true
 //             addReversedWordToResult()
@@ -63,11 +28,46 @@ const answer = (str) => {
 //             if(inTag) {
 //                 result += s
 //             }else {
-//                 stack.push(s)
+//                 word.push(s)
 //             }
 //         }
 //     }
 //     addReversedWordToResult()
 //     return result
 // };
+
+const answer = (str) => {
+    let result = ''
+    let stack = []
+    let inTag = false
+
+    const addReversedWordToResult = () => {
+        while (stack.length > 0) {
+            result += stack.pop()
+        }
+    };
+
+    for (let i = 0; i < str.length; i++) {
+        const s = str[i]
+        if(s === '<') {
+            inTag = true
+            addReversedWordToResult()
+            result += s
+        } else if(s === '>') {
+            inTag = false
+            result += s
+        } else if(s === ' '){
+            addReversedWordToResult()
+            result += s
+        } else {
+            if(inTag) {
+                result += s
+            }else {
+                stack.push(s)
+            }
+        }
+    }
+    addReversedWordToResult()
+    return result
+};
 console.log(answer(input[2]));
