d3e234b91af4fd8e42c65a11ce0b97fddd78fa5a
Turtleman <seungkyu0215@gmail.com>
2024-06-15 22:58:29 +0900

    스택 정리

--- 스택/2_1918_후위표기식.js
+++ 스택/2_1918_후위표기식.js
@@ -4,38 +4,70 @@ const input = fs.readFileSync('../example.txt')
     .trim()
     .split('\n')
 
+// 스택은 연산자와 괄호가 들어가게 된다.
 let stack = []
 let answer = ''
-Array(...input[0]).forEach(str => {
-    if (str === '(') {
-        stack.push(str);
-    } else if (str === ')') {
-        // 스택에서 여는 괄호를 만날 때까지 팝하여 결과에 추가
-        while (stack.length && stack[stack.length - 1] !== '(') {
-            answer += stack.pop();
+const priority = {
+    '*': 1,
+    '/': 1,
+    '+': 2,
+    '-': 2,
+    '(': 3,
+}
+Array(...input[0])
+    .forEach(str => {
+        if (str === '(') {
+            stack.push(str);
+        } else if (str === ')') {
+            // 괄호는 닫히면 처음 괄호까지 모두 꺼낸다.
+            while (stack.length && stack[stack.length - 1] !== '(') {
+                answer += stack.pop();
+            }
+            stack.pop();
+        } else if (str in priority) {
+            // 스택 맨 위에 있는 값의 우선순위가 높은 경우 모두 뺴준다.
+            // 우선순위가 높으면 계속 스택에 넣고 우선순이가 낮으면 빼준다.
+            // 즉 +,-,( 값은 우선순위가 높은 *,/ 만나면 모두 제거 된다.
+            while (stack.length &&
+            priority[str] >= priority[stack[stack.length-1]]) {
+                answer += stack.pop();
+            }
+            stack.push(str);
+        } else {
+            answer += str;
         }
-        // 여는 괄호를 스택에서 제거
-        stack.pop();
-    } else if (str === '*' || str === '/') {
-        // 스택의 탑에 곱셈 또는 나눗셈 연산자가 있으면 팝하여 결과에 추가
-        // 동일한 우선순위의 연산자('*'나 '/')가 스택의 탑에 있는 동안 팝하여 결과에 추가합니다.
-        while (stack.length && (stack[stack.length - 1] === '*' || stack[stack.length - 1] === '/')) {
-            answer += stack.pop();
-        }
-        // 현재 연산자를 스택에 추가
-        stack.push(str);
-    } else if (str === '+' || str === '-') {
-        // 여는 괄호 '('를 만날 때까지 스택에서 팝하여 결과에 추가합니다.
-        // 이는 덧셈과 뺄셈이 곱셈과 나눗셈보다 낮은 우선순위를 갖기 때문입니다.
-        while (stack.length && stack[stack.length - 1] !== '(') {
-            answer += stack.pop();
-        }
-        // 현재 연산자를 스택에 추가
-        stack.push(str);
-    } else {
-        answer += str;
-    }
 })
+['*', '(']
+// Array(...input[0]).forEach(str => {
+//     if (str === '(') {
+//         stack.push(str);
+//     } else if (str === ')') {
+//         // 스택에서 여는 괄호를 만날 때까지 팝하여 결과에 추가
+//         while (stack.length && stack[stack.length - 1] !== '(') {
+//             answer += stack.pop();
+//         }
+//         // 여는 괄호를 스택에서 제거
+//         stack.pop();
+//     } else if (str === '*' || str === '/') {
+//         // 스택의 탑에 곱셈 또는 나눗셈 연산자가 있으면 팝하여 결과에 추가
+//         // 동일한 우선순위의 연산자('*'나 '/')가 스택의 탑에 있는 동안 팝하여 결과에 추가합니다.
+//         while (stack.length && (stack[stack.length - 1] === '*' || stack[stack.length - 1] === '/')) {
+//             answer += stack.pop();
+//         }
+//         // 현재 연산자를 스택에 추가
+//         stack.push(str);
+//     } else if (str === '+' || str === '-') {
+//         // 여는 괄호 '('를 만날 때까지 스택에서 팝하여 결과에 추가합니다.
+//         // 이는 덧셈과 뺄셈이 곱셈과 나눗셈보다 낮은 우선순위를 갖기 때문입니다.
+//         while (stack.length && stack[stack.length - 1] !== '(') {
+//             answer += stack.pop();
+//         }
+//         // 현재 연산자를 스택에 추가
+//         stack.push(str);
+//     } else {
+//         answer += str;
+//     }
+// })
 // 스택에 남아 있는 모든 연산자를 팝하여 결과에 추가
 while (stack.length) {
     answer += stack.pop();
--- 중간고사/4번_2504_괄호의값.js
+++ 중간고사/4번_2504_괄호의값.js
@@ -1,47 +1,54 @@
 const fs = require('fs')
-const input = fs.readFileSync('../example.txt')
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../example.txt'
+const input = fs.readFileSync(filePath)
     .toString()
     .trim()
     .split('\n')
 
-const stack = []
-Array(...input[0]).forEach(s => {
-    if (s === ')') {
-        let temp = 0;
-        while (stack.length > 0) {
-            let top = stack.pop();
-            if (top === '(') {
-                if (temp === 0) {
-                    stack.push(2);
-                } else {
-                    stack.push(2 * temp);
-                }
-                break;
-            } else if (typeof top === 'number') {
-                temp += top;
-            }
+const stack = [];
+for (const p of input[0]) {
+    if (p === ")") {
+        let result = 0;
+        let top = 0;
+
+        // 열린 괄호를 만날때까지 숫자를 더한다.
+        while (Number.isInteger(top)) {
+            result += top;
+            top = stack.pop();
         }
-    } else if (s === ']') {
-        let temp = 0;
-        while (stack.length > 0) {
-            let top = stack.pop();
-            if (top === '[') {
-                if (temp === 0) {
-                    stack.push(3);
-                } else {
-                    stack.push(3 * temp);
-                }
-                break;
-            } else if (typeof top === 'number') {
-                temp += top;
-            }
+
+        // 탑이 닫힌 괄호면 정상
+        if (top === "(") {
+            // result 값이 0이면 괄호 내부에 값이 존재하지 않음 즉 기본 괄호 ()임
+            // 그렇지 않으면 괄호 안에 값이 존재한다는 뜻으로 *2 대상
+            stack.push(result ? result * 2 : 2);
+        } else {
+            // 만약 다른 괄호가 나오는 경우 옳바른 괄호가 아니므로 0 출력
+            console.log(0);
+            return;
         }
-    } else {
-        stack.push(s)
     }
-})
+    else if (p === "]") {
+        let temp = 0;
+        let top = 0;
+        while (Number.isInteger(top)) {
+            temp += top;
+            top = stack.pop();
+        }
+        if (top === "[") {
+            stack.push(temp ? temp * 3 : 3);
+        } else {
+            console.log(0);
+            return;
+        }
+    }else {
+        stack.push(p)
+    }
+}
 
-console.log(stack.reduce((a,c) => a + c, 0))
 
-
-
+console.log(
+    stack
+        .filter(s => !Number.isInteger(s))
+        .length > 0 ? 0 : stack.reduce((acc,cur)=>acc+cur)
+);
